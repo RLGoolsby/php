dba7daa869a35ddc48e0db9df015cad7567d2b48
Rhonda Goolsby <rlgoolsby@gmail.com>
2016-12-12 15:13:21 -0500

    Add edits

--- index.php
+++ index.php
@@ -10,6 +10,7 @@ $courses = $json_data['courses']['completed'];
 <html>
   <head>
     <meta charset="utf-8">
+    <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
     <link href='https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300' rel='stylesheet' type='text/css'>
