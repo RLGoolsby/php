58c428b0d302baa7168909e74fa9ef904459169f
Rhonda Goolsby <rlgoolsby@gmail.com>
2016-12-13 15:07:51 -0500

    Add flexbox styling

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ $courses = $json_data['courses']['completed'];
 <html>
   <head>
     <meta charset="utf-8">
-    <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
     <link href='https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300' rel='stylesheet' type='text/css'>
@@ -37,7 +37,7 @@ $courses = $json_data['courses']['completed'];
             <?php
               foreach ($courses as $course) {
                 echo '<li class= "col">';
-                echo '<img height= "200px" src= "' .  $course["badge"] . '"/><br>';
+                echo '<img height= "210px" src= "' .  $course["badge"] . '"/><br>';
                 echo '<a href= "' . $course["url"] . '">' . $course["title"] . '</a>';
                 echo '</li>';
               }
