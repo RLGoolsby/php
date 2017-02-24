2ca1cff9e88ec8c73a867c4c6dd791a70709ec78
Rhonda Goolsby <rlgoolsby@gmail.com>
2017-02-24 15:14:18 -0500

    Add bootstrap for responsiveness

--- index.php
+++ index.php
@@ -14,15 +14,20 @@ $courses = $json_data['courses']['completed'];
 <html lang = "en">
   <head>
     <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
     <link href='https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300' rel='stylesheet' type='text/css'>
     <link href='https://fonts.googleapis.com/css?family=Lato:400,100,700' rel='stylesheet' type='text/css'>
-    <link rel="stylesheet" href="assets/css/styles.css">
+
+    <!-- BOOTSTRAP -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
+    integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
     <!-- the is the title that appears in the browser tab -->
     <title>"<?php echo $json_data['user']['username']; ?>" 's badges</title>
-
+    <link rel="stylesheet" href="assets/css/styles.css">
   </head>
 
 
@@ -30,18 +35,19 @@ $courses = $json_data['courses']['completed'];
 
   <body>
 
-
+    <div class= "container-fluid">
       <header>
         <h1> My Badges</h1>
       </header>
 
-      <div class= "container">
+
+        <div class= "row">
         <ul class= "badges">
           <!--The is the php code that GETS data from the server json and plugs it into my html -->
               <?php
                 foreach ($courses as $course) {
-                  echo '<li class= "col">';
-                  echo '<img height= "210px" src= "' .  $course["badge"] . '"/><br>';
+                  echo '<li class= "col col-xs-6 col-md-4">';
+                  echo '<img height= "240px" src= "' .  $course["badge"] . '"/><br>';
                   echo '<a href= "' . $course["url"] . '">' . $course["title"] . '</a>';
                   echo '</li>';
                 }
@@ -49,5 +55,7 @@ $courses = $json_data['courses']['completed'];
               ?>
         </ul>
       </div>
+    </div>
+      <script src="assets/js/bootstrap.min.js"></script>
   </body>
 </html>
