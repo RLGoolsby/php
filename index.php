21678a897f3ac12c72651450b9ae22f7628baa30
Rhonda Goolsby <rlgoolsby@gmail.com>
2017-03-17 13:09:46 -0400

    Styling changes to header

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ $courses = $json_data['courses']['completed'];
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
@@ -66,7 +66,7 @@ $courses = $json_data['courses']['completed'];
               <?php
                 foreach ($courses as $course) {
 
-                  echo '<li class= "col">';
+                  echo '<li class= "col span12">';
                   echo '<img height= "210px" src= "' .  $course["badge"] . '"/><br>';
                   echo '<a href= "' . $course["url"] . '">' . $course["title"] . '</a>';
                   echo '</li>';
